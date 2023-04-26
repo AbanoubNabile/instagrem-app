ff9f35c1398908c6bf4f79a3a8c7f7e6a1ac65c8
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-26 22:07:49 +0200

    add appbar to mobile screen

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -10,6 +10,8 @@ class mobileScreen extends StatefulWidget {
 class _mobileScreenState extends State<mobileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
