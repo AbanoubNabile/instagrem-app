dbbcc4c646c164d9e16eb3a35d136964e9fca41b
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-26 21:53:45 +0200

    add widget web screen to file web

--- lib/responsive/web.dart
+++ lib/responsive/web.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class webScereen extends StatefulWidget {
+  const webScereen({super.key});
+
+  @override
+  State<webScereen> createState() => _webScereenState();
+}
+
+class _webScereenState extends State<webScereen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
