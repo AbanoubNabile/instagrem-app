ecc47e8a680c97729d6974f3e0a9be6a9491fa07
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-26 22:52:25 +0200

    create widget resposive in file resposive

--- lib/responsive/respobsive.dart
+++ lib/responsive/respobsive.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class Resposive extends StatefulWidget {
+  const Resposive({super.key});
+
+  @override
+  State<Resposive> createState() => _ResposiveState();
+}
+
+class _ResposiveState extends State<Resposive> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
