36333acd9d68ac3716c2337da8f015de29cff425
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-26 22:02:38 +0200

    add widget to mobile file

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class mobileScreen extends StatefulWidget {
+  const mobileScreen({super.key});
+
+  @override
+  State<mobileScreen> createState() => _mobileScreenState();
+}
+
+class _mobileScreenState extends State<mobileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
