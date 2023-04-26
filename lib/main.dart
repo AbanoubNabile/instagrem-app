d5b8e0e6550350efd083b66736c3f8fb9ba262a7
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-26 23:29:01 +0200

    run web screen and mobile screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Resposive(),
+      home: Resposive(
+        mymobileScreen: mobileScreen(),
+        mywebScreen: webScereen(),
+      ),
     );
   }
 }
