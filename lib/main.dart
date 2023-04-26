fdb4c321cf705ab7240e018b63a6da9c45f9bb1f
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-26 23:49:34 +0200

    add theme data

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ,
+      theme: ThemeData(),
       home: Resposive(
         mymobileScreen: mobileScreen(),
         mywebScreen: webScereen(),
