fad389afb4e2b9d29d25c481f1f0ce143c90299e
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-27 21:54:45 +0200

    add widget to profile screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class search extends StatefulWidget {
+  const search({super.key});
 
+  @override
+  State<search> createState() => _searchState();
+}
+
+class _searchState extends State<search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
