3254dcc47323fbcc8888d6e9d603a46039638dfe
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-27 23:16:27 +0200

    add heigth to svg picture

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,6 +18,7 @@ class _homeState extends State<home> {
           title: SvgPicture.asset(
             "assets/img/instagram.svg",
             color: primaryColor,
+            height: 32,
           )),
     );
   }
